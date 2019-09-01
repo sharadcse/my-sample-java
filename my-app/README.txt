a87ab6e5c481eb3eebeeed1519ca1bebca5086a4
Sharad Chandra <sharachandra@paypal.com>
2019-09-01 21:24:01 +0530

    Compiled code

---
+++ my-app/README.txt
@@ -0,0 +1,1 @@
+java -cp target/my-app-1.0-SNAPSHOT.jar:$HOME/.m2/repository/org/json/json/20160810/json-20160810.jar com.sharad.app.jsonobject
--- my-app/src/main/java/com/sharad/app/jsonobject.java
+++ my-app/src/main/java/com/sharad/app/jsonobject.java
@@ -4,36 +4,38 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;;
 
 public class jsonobject {
 
     public static void main(String[] args) {
 
-
         String sample1 = "{\"took\": 16,\"errors\": false,\"items\": [{\"index\": {\"_seq_no\": 109,\"_type\": \"summarytransaction\",\"_version\": 27,\"_shards\": {\"total\": 2,\"successful\": 2,\"failed\": 0},\"_id\": \"1\",\"_index\": \"cs_transaction_index\",\"result\": \"updated\",\"_primary_term\": 1,\"status\": 200}}]}";
+        JSONObject jsample1 = new JSONObject(sample1);
+        System.out.println(jsample1);
 
-        Response datastreamservResponse = Response
-                .status(200)
-                .type(MediaType.APPLICATION_JSON)
-                .entity(sample1)
-                .build();
+        // Response datastreamservResponse = Response
+        //         .status(200)
+        //         .type(MediaType.APPLICATION_JSON)
+        //         .entity(sample1)
+        //         .build();
+
+        // System.out.println(datastreamservResponse.toString());
 
         JSONObject obj = new JSONObject(sample1);
-        // obj.put("name", "mkyong.com");
-        // obj.put("age", 100);
+        obj.put("name", "mkyong.com");
+        obj.put("age", 100);
 
-        // JSONObject obj1 = new JSONObject();
-        // obj1.put("name", "com");
-        // obj1.put("age", 10);
+        JSONObject obj1 = new JSONObject();
+        obj1.put("name", "com");
+        obj1.put("age", 10);
 
-        // JSONArray list = new JSONArray();
-        // list.put(obj1);
+        JSONArray list = new JSONArray();
+        list.put(obj1);
 
-        // obj.put("messages", list);
+        obj.put("messages", list);
 
-
-        System.out.print(obj);
-
+        System.out.println(obj);
     }
 
 }
