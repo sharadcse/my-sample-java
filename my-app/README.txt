7504ed0190dd22d4342cd01e7631d5866c0a9a3b
Sharad Chandra <sharachandra@paypal.com>
2019-09-01 23:17:45 +0530

    Compiled code

--- my-app/README.txt
+++ my-app/README.txt
@@ -1,1 +1,6 @@
+mvn compile;
+
+mvn exec:java -Dexec.mainClass="com.sharad.app.jsonobject" -Dexec.classpathScope=runtime
+
 java -cp target/my-app-1.0-SNAPSHOT.jar:$HOME/.m2/repository/org/json/json/20160810/json-20160810.jar com.sharad.app.jsonobject
+
--- my-app/src/main/java/com/sharad/app/jsonobject.java
+++ my-app/src/main/java/com/sharad/app/jsonobject.java
@@ -1,10 +1,17 @@
 package com.sharad.app;
 
+import java.util.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.MediaType;;
+import javax.ws.rs.core.MediaType;
+
+// import org.elasticsearch.action.index.IndexRequestBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.MatchAllQueryBuilder;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.Operator;
 
 public class jsonobject {
 
@@ -12,7 +19,8 @@ public class jsonobject {
 
         String sample1 = "{\"took\": 16,\"errors\": false,\"items\": [{\"index\": {\"_seq_no\": 109,\"_type\": \"summarytransaction\",\"_version\": 27,\"_shards\": {\"total\": 2,\"successful\": 2,\"failed\": 0},\"_id\": \"1\",\"_index\": \"cs_transaction_index\",\"result\": \"updated\",\"_primary_term\": 1,\"status\": 200}}]}";
         JSONObject jsample1 = new JSONObject(sample1);
-        System.out.println(jsample1);
+        System.out.println(jsample1.get("took"));
+        System.out.println(jsample1.getJSONArray("items").getJSONObject(0));
 
         // Response datastreamservResponse = Response
         //         .status(200)
@@ -21,6 +29,25 @@ public class jsonobject {
         //         .build();
 
         // System.out.println(datastreamservResponse.toString());
+
+
+        // StringBuilder
+
+        String actionMetaData = String.format("{ \"index\" : { \"_index\" : \"%s\", \"_id\" : \"%s\" } }%n", "index", "_id");
+
+        List<String> bulkData = new ArrayList<>(); // a list of your documents in JSON strings
+        bulkData.add("one");
+        bulkData.add("two");
+        bulkData.add("three");
+        StringBuilder bulkRequestBody = new StringBuilder();
+        for (String bulkItem : bulkData) {
+            bulkRequestBody.append(actionMetaData);
+            bulkRequestBody.append(bulkItem);
+            bulkRequestBody.append("\n");
+        }
+        System.out.println(bulkRequestBody.toString());
+
+        // JSONObject
 
         JSONObject obj = new JSONObject(sample1);
         obj.put("name", "mkyong.com");
@@ -36,6 +63,22 @@ public class jsonobject {
         obj.put("messages", list);
 
         System.out.println(obj);
+
+
+        // Query builder
+        MatchAllQueryBuilder maqb = QueryBuilders
+                .matchAllQuery();
+
+        Map <String, Float> fields = new HashMap<>();
+        fields.put("fields1", 8.0f);
+        MultiMatchQueryBuilder mmqb = QueryBuilders.multiMatchQuery("-1200 Amount", "transactionType","counterPartyName","netAmount.amount")
+        .field("field1")
+        .fields(fields)
+        .operator(Operator.AND);
+
+        String searchQuery = "{ \"query\" : %s }\n";
+        System.out.println("qb======" + String.format(searchQuery, maqb.toString()));
+        System.out.println("mmqb======" + String.format(searchQuery, mmqb.toString()));
     }
 
 }
